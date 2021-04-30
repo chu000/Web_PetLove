6b946922dbb859161a19369e4117d93d894a7789
chu000 <2018g011117@gmail.com>
2021-04-30 17:39:26 +0800

    信用卡卡號  不隱藏

--- js/Donate/donate.js
+++ js/Donate/donate.js
@@ -44,26 +44,29 @@ $(".c_sameName02").on( "change",function (event) {
 });
 
 // 自動填入-----------------------------------------------
-$(".c_donate_banner_title").click( function () {
+$(".c_foot_auto").click( function () {
   $('.c_fast_money').val("500");
   $('.c_fast_selecthome').prop('selectedIndex', 2);
   // $('.c_donate_address ').find('.form-control').prop('selectedIndex', 2);
 
-  $('.c_fast_donationNAME').val("心鐘");
+  $('.c_fast_donationNAME').val("阮心鐘曼");
   $('.c_fast_IDnumber').val("T222567123");
   $('.c_fast_cellphone').val("0912987654");
-  $('.c_fast_email').val("GG@gmail.com");
-  $('.c_fast_receiptNAME').val("阮曼");
-  $('.c_fast_addressNAME').val("阮曼");
+  $('.c_fast_email').val("happymama@gmail.com");
+  $('.c_fast_receiptNAME').val("阮心鐘曼");
+  $('.c_fast_addressNAME').val("阮心鐘曼");
   $('.c_fast_address').val("松勇路69巷2號");
-  $('.c_fast_card_number01').val("7831");
-  $('.c_fast_card_number02').val("7204");
-  $('.c_fast_card_number03').val("2751");
-  $('.c_fast_card_number04').val("9437");
+  $('.c_fast_card_number01').val("5412");
+  $('.c_fast_card_number02').val("3456");
+  $('.c_fast_card_number03').val("7890");
+  $('.c_fast_card_number04').val("1232");
   $('.c_fast_period01').val("23");
   $('.c_fast_period02').val("10");
   $('.c_fast_security').val("603");
   $('.c_fast_cardName').val("心鐘");
+  $('.mastercard').css("fill","#89A7C2");
+
+  
 
 });
 
